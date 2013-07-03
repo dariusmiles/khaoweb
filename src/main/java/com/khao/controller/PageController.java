b18a6d903a3fcbee8a48eb0214510cd27db6900f
karlbarth.alejandro@yahoo.com <>
2013-07-03 06:18:55 +0000

    remove inline styling and move to separate css file

--- src/main/java/com/khao/controller/PageController.java
+++ src/main/java/com/khao/controller/PageController.java
@@ -1,5 +1,29 @@
 package com.khao.controller;
 
-public class PageController
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class PageController extends HttpServlet
 {
+  @Override
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws ServletException, IOException {
+      
+    // Use "request" to read incoming HTTP headers (e.g. cookies)
+    // and HTML form data (e.g. data the user entered and submitted)
+    
+    // Use "response" to specify the HTTP response line and headers
+    // (e.g. specifying the content type, setting cookies).
+    
+    PrintWriter out = response.getWriter();
+    // Use "out" to send content to browser
+    out.println("<h1>HELLO</h1>");
+  }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,18 +1,11 @@
 <html>
   <head>
-    <style>
-      .main{
-        width: 800px;
-        height: 800px;
-        margin: auto;
-        background-color: lightblue;
-      }
-    </style>
+    <link rel="stylesheet" type="text/css" href="stylesheet/default.css"/>
   </head>
 <body>
 <h2>Hello World!</h2>
   <div class="main">
-  
+    <a href="page">Hello</a>
   </div>
 </body>
 </html>
